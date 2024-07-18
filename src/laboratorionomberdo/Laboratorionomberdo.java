4087d69fe7b09badd74f1a2403a1f164c7720929
Marian Echeverria <mariandreed@outlook.es>
2024-07-18 12:21:05 -0600

    repositoriolab

---
+++ src/laboratorionomberdo/Laboratorionomberdo.java
@@ -0,0 +1,17 @@
+package laboratorionomberdo;
+
+public class Laboratorionomberdo {
+
+    public static void main(String[] args) {
+        int matriz[][] = {{5, 7, 9}, {4, 2, 1}, {1, 1, 1}};
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {               
+                System.out.print(matriz[i][j] + ", ");
+            }
+         
+        }
+      
+            
+        
+    }
+}
